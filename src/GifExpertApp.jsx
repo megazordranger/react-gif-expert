1e25eba0011ee5dd167fa1fa0cd12321bffb055d
Megazord <megazord044@gmail.com>
2023-03-10 18:23:32 -0400

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One punch"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    // setCategories([newCategory, ...categories]);
+    setCategories((categories) => [newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
